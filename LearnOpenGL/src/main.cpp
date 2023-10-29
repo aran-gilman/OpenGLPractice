3caeb42c58dbfdd4c3cf191011476800d19ac521
Aran Gilman <f.zoniventris@gmail.com>
2023-10-28 18:52:03 -0700

    Pass color from vertex shader to fragment

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -6,19 +6,25 @@
 const char* vertexShaderSource = R"shader(
 #version 330 core
 layout (location = 0) in vec3 aPos;
+out vec4 vertexColor;
+
 void main()
 {
     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-})shader";
+    vertexColor = vec4(0.5, 0.0, 0.0, 1.0);
+}
+)shader";
 
 const char* fragmentShaderSource = R"shader(
 #version 330 core
 out vec4 FragColor;
+in vec4 vertexColor;
 
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-})shader";
+    FragColor = vertexColor;
+}
+)shader";
 
 
 const char* yellowFragmentShaderSource = R"shader(
