e447f84cea68ea5c06c74ea83420b582f8f36a35
Aran Gilman <f.zoniventris@gmail.com>
2023-10-29 13:22:52 -0700

    Move vertex attrib code to Mesh

--- LearnOpenGL/src/Mesh.cpp
+++ LearnOpenGL/src/Mesh.cpp
@@ -15,6 +15,17 @@ Mesh::Mesh(const std::vector<float>& vertices, const std::vector<unsigned int>& 
 	glGenBuffers(1, &eboID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
 
 Mesh::~Mesh()
--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -108,18 +108,6 @@ int main()
 	};
 	Mesh mesh(vertices, indices);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
 	Vector3 position(0, 0, 0);
 
 	while (!glfwWindowShouldClose(window))
