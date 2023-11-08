d035fc0b79a4dafeca01cd8e6c8cabc51dfc6434
Aran Gilman <f.zoniventris@gmail.com>
2023-11-08 12:10:03 -0800

    Set directional light color to white

--- LearnOpenGL/src/Game.cpp
+++ LearnOpenGL/src/Game.cpp
@@ -87,7 +87,7 @@ Game::Game() :
 
 	Object* scene = CreateObject();
 	scene->AddComponent<AmbientLight>(0.2f, Color{1.0f, 1.0f, 1.0f, 1.0f })->Use(ambientLightBuffer.GetID());
-	scene->AddComponent<DirectionalLight>(1.0f, Color{ 0.0f, 1.0f, 1.0f, 1.0f }, glm::vec3(1.0f, 0.0f, 0.0f))->Use(directionalLightBuffer.GetID());
+	scene->AddComponent<DirectionalLight>(1.0f, Color{ 1.0f, 1.0f, 1.0f, 1.0f }, glm::vec3(1.0f, 0.0f, 0.0f))->Use(directionalLightBuffer.GetID());
 
 	window.OnUpdate().Register(std::bind_front(&Game::HandleUpdate, this));
 	window.OnKeyInput().Register(std::bind_front(&Game::HandleKeyInput, this));
